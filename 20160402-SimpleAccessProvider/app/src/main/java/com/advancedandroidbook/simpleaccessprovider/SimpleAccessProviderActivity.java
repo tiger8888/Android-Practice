84ab509f9491773e0cd7b29df20b68f7481e2638
martinsuchen35 <chensu@chensu-maci.dhcp.mathworks.com>
2016-06-07 17:58:16 -0400

    Refactor

--- 20160402-SimpleAccessProvider/app/src/main/java/com/advancedandroidbook/simpleaccessprovider/SimpleAccessProviderActivity.java
+++ 20160402-SimpleAccessProvider/app/src/main/java/com/advancedandroidbook/simpleaccessprovider/SimpleAccessProviderActivity.java
@@ -10,23 +10,33 @@ import android.support.v4.widget.SimpleCursorAdapter;
 public class SimpleAccessProviderActivity extends Activity {
 
     private Uri mUri = Uri.parse("content://com.advancedandroidbook.simplesearchprovider.SimpleFieldnotesContentProvider/fieldnotes_provider");
+    private Cursor mCursor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.list_view);
 
-        Cursor cursor = getContentResolver().query(mUri, null, null, null, "fieldnotes_title");
+        mCursor = getContentResolver().query(mUri, null, null, null, "fieldnotes_title");
 
         ListView listView = (ListView) findViewById(R.id.provider);
         SimpleCursorAdapter adapter = new SimpleCursorAdapter(
                 this,
                 android.R.layout.two_line_list_item,
-                cursor,
+                mCursor,
                 new String[] { "FIELDNOTES_TITLE", "FIELDNOTES_BODY" },
                 new int[] { android.R.id.text1, android.R.id.text2 },
                 0
         );
         listView.setAdapter(adapter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (mCursor != null) {
+            mCursor.close();
+        }
+
+        super.onDestroy();
     }
 }
